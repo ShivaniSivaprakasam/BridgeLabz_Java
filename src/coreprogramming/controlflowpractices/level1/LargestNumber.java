8af44e4e94d1882cb235f9d8763bdf23a0cc9df6
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:15:23 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level1/LargestNumber.java
+++ src/coreprogramming/controlflowpractices/level1/LargestNumber.java
@@ -1,26 +1,50 @@
+/**
+ * Program Name: LargestNumber
+ *
+ * Description:
+ * This program reads three integer numbers from the user and determines
+ * which number is the largest. If more than one number is equal and largest,
+ * it prints a message stating that more than one number are equal.
+ *
+ * Steps:
+ * 1. Input three integers from the user.
+ * 2. Use if-else statements to compare the numbers and find the largest.
+ * 3. Display the result.
+ *
+ * Concepts Used:
+ * - Conditional statements (if-else)
+ * - Logical operators (&&)
+ * - Scanner for user input
+ */
 package coreprogramming.controlflowpractices.level1;
-import  java.util.Scanner;
+
+import java.util.Scanner;
+
 public class LargestNumber {
+
     public static void main(String[] args) {
+
+        // Creating Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
-        // Getting user input
+
+        // Getting three numbers from the user
         System.out.print("Enter first number: ");
         int number1 = sc.nextInt();
+
         System.out.print("Enter second number: ");
         int number2 = sc.nextInt();
+
         System.out.print("Enter third number: ");
         int number3 = sc.nextInt();
-        // Initialization of loop to find the largest number
-        if ( number1 > number2 && number1 > number3){
+
+        // Determine the largest number using if-else statements
+        if (number1 > number2 && number1 > number3) {
             System.out.println("Is the first number " + number1 + " the largest? Yes");
-        }
-        else if (number2 > number1 && number2 > number3){
+        } else if (number2 > number1 && number2 > number3) {
             System.out.println("Is the second number " + number2 + " the largest? Yes");
-        }
-        else if (number3 > number1 && number3 > number2){
+        } else if (number3 > number1 && number3 > number2) {
             System.out.println("Is the third number " + number3 + " the largest? Yes");
-        }
-        else{
+        } else {
             System.out.println("Out of the three entered numbers more than one number are equal.");
         }
     }
