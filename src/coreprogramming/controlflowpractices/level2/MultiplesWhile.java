a35bf3810fe47039575c4bfc9a26ed8d8f565661
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:39:47 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/MultiplesWhile.java
+++ src/coreprogramming/controlflowpractices/level2/MultiplesWhile.java
@@ -1,24 +1,42 @@
 package coreprogramming.controlflowpractices.level2;
-import  java.util.Scanner;
+
+import java.util.Scanner;
+
 public class MultiplesWhile {
+
+    /*
+     * This program finds all divisors (factors) of a number entered by the user
+     * using a while loop.
+     * Steps:
+     * 1. Take a positive number input less than or equal to 100.
+     * 2. Use a while loop to check all numbers less than the input for divisibility.
+     * 3. Print all numbers that divide the input evenly.
+     */
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        Scanner input = new Scanner(System.in); // Scanner for user input
+
+        // Prompt user to enter a number
         System.out.print("Enter a number: ");
         int number = input.nextInt();
-        // Setting condition to get onlu number between 0 and 100
-        while ( number > 100 || number < 0 ) {
-            System.out.print("Enter a number less than 100 and should also be positive: ");
+
+        // Input validation: number must be positive and ≤ 100
+        while (number < 0 || number > 100) {
+            System.out.print("Enter a number less than 100 and positive: ");
             number = input.nextInt();
         }
-        int counter = number - 1;
-        // Setting condition to get counter value more than 1
-        while (counter > 1) {
-            if (number % counter == 0) {
+
+        System.out.println("The divisors of " + number + " are:");
+
+        int counter = number; // Start from the number itself
+
+        // Loop to find all divisors using while
+        while (counter >= 1) { // Include 1 as a factor
+            if (number % counter == 0) { // Check divisibility
                 System.out.println(counter);
             }
-            // Decrementing counter
-            counter--;
+            counter--; // Decrement counter
         }
     }
 }
