04081f859ee96f2d3fe73bcf8d42bdee0519b6a1
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:05:39 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/IntOperation.java
+++ src/coreprogramming/programmingelements/level2/IntOperation.java
@@ -1,18 +1,36 @@
 package coreprogramming.programmingelements.level2;
+
 import java.util.Scanner;
+
+/*
+ * This program performs different arithmetic
+ * operations using integer values.
+ */
 public class IntOperation {
+
     public static void main(String[] args) {
+
+        // Taking integer input values from the user
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter first number: ");
-        int a =  input.nextInt();
+        int a = input.nextInt();
+
         System.out.print("Enter second number: ");
-        int b =  input.nextInt();
+        int b = input.nextInt();
+
         System.out.print("Enter third number: ");
         int c = input.nextInt();
-        int operation1 = a + b *c;
+
+        // Performing arithmetic operations
+        int operation1 = a + b * c;
         int operation2 = a * b + c;
         int operation3 = c + a / b;
         int operation4 = a % b + c;
-        System.out.println("The results of the Int Operations are " +operation1 +" , " +operation2 +" , " +operation3 +" and " +operation4 +".");
+
+        // Displaying the results
+        System.out.println("The results of the Int Operations are " +
+                operation1 + " , " + operation2 + " , " +
+                operation3 + " and " + operation4 + ".");
     }
 }
