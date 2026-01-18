7eca1cbc22cc4b25774d73ea24cf07440e7a496f
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 13:30:02 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/javamethods/level1/IntegerNumber.java
+++ src/coreprogramming/javamethods/level1/IntegerNumber.java
@@ -1,22 +1,39 @@
 package coreprogramming.javamethods.level1;
-import java.util.Scanner;
-public class IntegerNumber {
-    public static void integerNum ( int number ){
-       if ( number > 0 ){
-           System.out.println("The number entered is positive.");
-       }
-       else if ( number < 0 ){
-           System.out.println("The number entered is negative.");
-       }
-       else{
-           System.out.println("The number entered is zero.");
-       }
-   }
-   public static void main(String[] args){
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number: ");
-       int number = input.nextInt();
-       integerNum( number );
-   }
 
+import java.util.Scanner;
+
+public class IntegerNumber {
+
+    /*
+     * This method checks whether the given number is
+     * positive, negative, or zero.
+     *
+     * Parameter:
+     * - number : an integer value entered by the user
+     */
+    public static void integerNum(int number) {
+
+        if (number > 0) {
+            System.out.println("The number entered is positive.");
+        } 
+        else if (number < 0) {
+            System.out.println("The number entered is negative.");
+        } 
+        else {
+            System.out.println("The number entered is zero.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Create Scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Read integer from user
+        System.out.print("Enter number: ");
+        int number = input.nextInt();
+
+        // Check the number type
+        integerNum(number);
+    }
 }
