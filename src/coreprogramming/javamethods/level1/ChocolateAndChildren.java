52858634c6947d2eac6dde56b79cee535b735674
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 13:27:48 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/javamethods/level1/ChocolateAndChildren.java
+++ src/coreprogramming/javamethods/level1/ChocolateAndChildren.java
@@ -1,19 +1,57 @@
 package coreprogramming.javamethods.level1;
-import java.util.Scanner;
-public class ChocolateAndChildren {
-    public static int[] childrenAndChocolate(int numberOfChocolates, int  numberOfChildren ){
-       int get = numberOfChocolates / numberOfChildren;
-       int left = numberOfChocolates % numberOfChildren;
-       return new int[]{get,left};
-   }
-   public static void main(String[] args){
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of children: ");
-       int numberOfChildren = input.nextInt();
-       System.out.print("Enter number of chocolate: ");
-       int numberOfChocolates = input.nextInt();
-       int[] outPut = childrenAndChocolate(numberOfChocolates,numberOfChildren);
-       System.out.println("The number of chocolates received and remaining are: " +outPut[1] +" and " +outPut[0]);
-   }
 
+import java.util.Scanner;
+
+public class ChocolateAndChildren {
+
+    /*
+     * This method calculates:
+     * 1. How many chocolates each child gets
+     * 2. How many chocolates are left after equal distribution
+     *
+     * Parameters:
+     * - numberOfChocolates : total chocolates available
+     * - numberOfChildren   : total number of children
+     *
+     * Returns:
+     * - An integer array where:
+     *   index 0 -> chocolates each child gets
+     *   index 1 -> remaining chocolates
+     */
+    public static int[] childrenAndChocolate(int numberOfChocolates, int numberOfChildren) {
+
+        // Chocolates each child gets
+        int get = numberOfChocolates / numberOfChildren;
+
+        // Chocolates remaining after distribution
+        int left = numberOfChocolates % numberOfChildren;
+
+        // Return results as an array
+        return new int[]{get, left};
+    }
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+
+        // Take number of children
+        System.out.print("Enter number of children: ");
+        int numberOfChildren = input.nextInt();
+
+        // Take number of chocolates
+        System.out.print("Enter number of chocolate: ");
+        int numberOfChocolates = input.nextInt();
+
+        // Call method and store result
+        int[] outPut = childrenAndChocolate(numberOfChocolates, numberOfChildren);
+
+        // Display result
+        System.out.println(
+                "Each child gets " + outPut[0] +
+                " chocolates and " + outPut[1] +
+                " chocolates are remaining."
+        );
+    }
 }
+
