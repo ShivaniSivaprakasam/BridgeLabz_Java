1b8fd7c7244eb9cc3053ec9ba9108e8af095b509
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:42:59 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/PowerWhile.java
+++ src/coreprogramming/controlflowpractices/level2/PowerWhile.java
@@ -1,26 +1,47 @@
 package coreprogramming.controlflowpractices.level2;
+
 import java.util.Scanner;
+
 public class PowerWhile {
+
+    /*
+     * This program calculates the power of a number using a while loop.
+     * Steps:
+     * 1. Take user input for a base number and exponent (power).
+     * 2. Validate that the base number is positive.
+     * 3. Use a while loop to multiply the base number by itself 'power' times.
+     * 4. Display the final result.
+     */
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // Getting user input
+
+        Scanner scan = new Scanner(System.in); // Scanner for user input
+
+        // Get base number
         System.out.print("Enter a number: ");
         int number = scan.nextInt();
+
+        // Get exponent
         System.out.print("Enter a power: ");
         int power = scan.nextInt();
-        // Setting condition to get positive number
+
+        // Validate base number to ensure it's positive
         while (number < 0) {
             System.out.print("Enter a positive number: ");
             number = scan.nextInt();
         }
+
         int result = 1;
         int counter = 0;
-        // Condition to get power of a number
-        while ( counter < power ){
-            result = number * result;
+
+        // Loop to calculate power using while
+        while (counter < power) {
+            result = result * number;
             counter++;
         }
-        // Displaying results
-        System.out.println(result);
+
+        // Display result
+        System.out.println(number + " raised to the power " + power + " is: " + result);
+
     }
 }
