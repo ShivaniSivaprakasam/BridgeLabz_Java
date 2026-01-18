ab01f680219c7a3bd6628ec085611db8d6a8d204
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:08:38 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/TotalIncome.java
+++ src/coreprogramming/programmingelements/level2/TotalIncome.java
@@ -1,14 +1,30 @@
 package coreprogramming.programmingelements.level2;
 
-import  java.util.Scanner;
+import java.util.Scanner;
+
+/*
+ * This program calculates the total income
+ * by adding salary and bonus.
+ */
 public class TotalIncome {
+
     public static void main(String[] args) {
+
+        // Taking salary input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter salary: ");
         float salary = input.nextFloat();
+
+        // Taking bonus input from the user
         System.out.print("Enter bonus: ");
         float bonus = input.nextFloat();
+
+        // Calculating total income
         float income = salary + bonus;
-        System.out.println("The salary is INR " + salary + " and bonus is INR " + bonus + "." + "Hence the total income is INR " + income + ".");
+
+        // Displaying salary, bonus, and total income
+        System.out.println("The salary is INR " + salary +
+                " and bonus is INR " + bonus +
+                ". Hence the total income is INR " + income + ".");
     }
 }
