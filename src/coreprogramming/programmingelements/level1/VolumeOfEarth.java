d31696d7ea745c8493b9a02d7da36a4d0906db88
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:02:20 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/VolumeOfEarth.java
+++ src/coreprogramming/programmingelements/level1/VolumeOfEarth.java
@@ -1,12 +1,31 @@
 package coreprogramming.programmingelements.level1;
 
+/*
+ * This program calculates the volume of the Earth
+ * in cubic kilometers and cubic miles.
+ */
 public class VolumeOfEarth {
+
     public static void main(String[] args) {
+
+        // Constant value of PI
         final double PI = 3.14;
+
+        // Radius of Earth in kilometers
         double radiusKm = 6378;
+
+        // Converting radius from kilometers to miles
         double radiusMi = 6378 * 1.6;
-        double volumeKm = (4/3) * PI * radiusKm;
-        double volumeMi = (4/3) * PI * radiusMi;
-        System.out.println("The volume of earth in cubic kilometers is " +volumeKm +" km^3" + " and cubic miles is " +volumeMi +" mi^3.");
+
+        /*
+         * Calculating volume of Earth in
+         * cubic kilometers and cubic miles
+         */
+        double volumeKm = (4 / 3) * PI * radiusKm;
+        double volumeMi = (4 / 3) * PI * radiusMi;
+
+        // Displaying the calculated volumes
+        System.out.println("The volume of earth in cubic kilometers is " +
+                volumeKm + " km^3 and cubic miles is " + volumeMi + " mi^3.");
     }
 }
