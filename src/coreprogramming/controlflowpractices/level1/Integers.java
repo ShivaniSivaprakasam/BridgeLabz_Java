0874a2e7c0ff1c05459b65edd880c96c5169b3ee
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:13:56 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level1/Integers.java
+++ src/coreprogramming/controlflowpractices/level1/Integers.java
@@ -1,19 +1,41 @@
+/**
+ * Program Name: Integers
+ *
+ * Description:
+ * This program reads an integer input from the user and determines whether
+ * the number is positive, negative, or zero. It then displays the result.
+ *
+ * Steps:
+ * 1. Input an integer from the user.
+ * 2. Use if-else statements to determine the sign of the number.
+ * 3. Display whether the number is Positive, Negative, or Zero.
+ *
+ * Concepts Used:
+ * - Conditional statements (if-else)
+ * - Scanner for user input
+ */
+
 package coreprogramming.controlflowpractices.level1;
+
 import java.util.Scanner;
+
 public class Integers {
+
     public static void main(String[] args) {
+
+        // Creating Scanner object to read input from the user
         Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        // Getting integer input from user
         System.out.print("Enter a number: ");
         int number = input.nextInt();
-        // Initialization of loop to determine the sign of the number
-        if ( number > 0 ){
+
+        // Determine the sign of the number
+        if (number > 0) {
             System.out.println("Positive.");
-        }
-        else if ( number < 0 ){
+        } else if (number < 0) {
             System.out.println("Negative.");
-        }
-        else{
+        } else {
             System.out.println("Zero.");
         }
     }
