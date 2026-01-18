4f7a8ca121c35ec2c6eea733c9a98abedf64365f
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:14:06 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/stringfunction/level1/LowerCase.java
+++ src/coreprogramming/stringfunction/level1/LowerCase.java
@@ -1,44 +1,75 @@
 package coreprogramming.stringfunction.level1;
+
 import java.util.Scanner;
+
+/*
+ * This program converts a string to lowercase
+ * using both a manual method and the in-built toLowerCase() method.
+ * It also compares the results of both methods.
+ */
 public class LowerCase {
-    public static String manual(String text){
+
+    /*
+     * This method converts a string to lowercase manually
+     * by adding 32 to uppercase ASCII characters.
+     */
+    public static String manual(String text) {
         String result = " ";
-        for ( int i = 0; i < text.length(); i++ ){
+        for (int i = 0; i < text.length(); i++) {
             char ch = text.charAt(i);
-            if ( ch >= 'A' && ch <= 'Z'){
-                ch = (char)(ch + 32);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + 32);
             }
             result += ch;
         }
         return result;
     }
-    public static String inBuilt(String text){
+
+    /*
+     * This method converts a string to lowercase
+     * using the built-in toLowerCase() method
+     */
+    public static String inBuilt(String text) {
         return text.toLowerCase();
     }
-    public static boolean check(String text1, String text2){
-        if ( text1.length() != text2.length() ){
+
+    /*
+     * This method checks if two strings are equal
+     * by comparing characters one by one
+     */
+    public static boolean check(String text1, String text2) {
+        if (text1.length() != text2.length()) {
             return false;
         }
-        for ( int i = 0; i < text1.length(); i++ ){
+        for (int i = 0; i < text1.length(); i++) {
             if (text1.charAt(i) != text2.charAt(i)) {
                 return false;
             }
         }
         return true;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        // Taking string input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String text = input.nextLine();
+
+        // Converting string to lowercase using both methods
         String text1 = manual(text);
         String text2 = inBuilt(text);
-        System.out.println("Lower case by manual method: " +text1);
-        System.out.println("Lower case by inbuilt method: " +text2);
-        if (check(text1, text2)){
-            System.out.println("\n the lower case string provided by both the methods are not the same.");
-        }
-        else{
-            System.out.println("\n the lower case string provided by both the methods are the same.");
+
+        // Displaying results
+        System.out.println("Lower case by manual method: " + text1);
+        System.out.println("Lower case by inbuilt method: " + text2);
+
+        // Comparing results of both methods
+        if (check(text1, text2)) {
+            System.out.println("\nThe lower case strings provided by both methods are the same.");
+        } else {
+            System.out.println("\nThe lower case strings provided by both methods are not the same.");
         }
     }
 }
+
