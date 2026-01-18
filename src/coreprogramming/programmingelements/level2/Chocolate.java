0d0afe047f557f587d8fbef4bc1de3b9c68eca23
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:03:44 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/Chocolate.java
+++ src/coreprogramming/programmingelements/level2/Chocolate.java
@@ -1,16 +1,34 @@
 package coreprogramming.programmingelements.level2;
 
 import java.util.Scanner;
+
+/*
+ * This program calculates how many chocolates
+ * each child gets and how many remain undistributed.
+ */
 public class Chocolate {
+
+    /*
+     * This method divides chocolates among children
+     * and prints the result.
+     */
     public static void divideChocolates(int numberOfChocolates, int numberOfChildren) {
+
+        // Calculating chocolates each child gets
         int eachChildGets = numberOfChocolates / numberOfChildren;
+
+        // Calculating remaining chocolates
         int remaining = numberOfChocolates % numberOfChildren;
 
-        System.out.println("The number of chocolates each child gets is " +eachChildGets +" and the number of remaining chocolates are " +remaining +".");
-
+        // Displaying the result
+        System.out.println("The number of chocolates each child gets is " +
+                eachChildGets + " and the number of remaining chocolates are " +
+                remaining + ".");
     }
 
     public static void main(String[] args) {
+
+        // Taking input from the user
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter number of chocolates: ");
@@ -19,6 +37,7 @@ public class Chocolate {
         System.out.print("Enter number of children: ");
         int numberOfChildren = input.nextInt();
 
+        // Calling method to divide chocolates
         divideChocolates(numberOfChocolates, numberOfChildren);
     }
 }
