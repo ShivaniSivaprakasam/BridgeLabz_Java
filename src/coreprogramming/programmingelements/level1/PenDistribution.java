4e882fd535f106e6b074590cd0dd616eb8ca98cf
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 17:58:58 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/PenDistribution.java
+++ src/coreprogramming/programmingelements/level1/PenDistribution.java
@@ -1,10 +1,27 @@
 package coreprogramming.programmingelements.level1;
+
+/*
+ * This program calculates how many pens each student gets
+ * and how many pens remain undistributed.
+ */
 public class PenDistribution {
+
     public static void main(String[] args) {
+
+        // Total number of pens available
         int totalPens = 14;
+
+        // Total number of students
         int totalStudents = 3;
-        int penDistribution = totalPens/totalStudents;
-        int remainder = totalPens %  totalStudents;
-        System.out.println("The Pen per student is " +penDistribution + " and the remaining pen not distributed is " + remainder +".");
+
+        // Calculating pens per student using integer division
+        int penDistribution = totalPens / totalStudents;
+
+        // Calculating remaining pens using modulus operator
+        int remainder = totalPens % totalStudents;
+
+        // Displaying the result
+        System.out.println("The Pen per student is " + penDistribution +
+                " and the remaining pen not distributed is " + remainder + ".");
     }
 }
