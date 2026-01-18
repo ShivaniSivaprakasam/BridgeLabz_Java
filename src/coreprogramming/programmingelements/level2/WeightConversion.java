28b4ebef2d57cca8db01975546bc17c42653dee4
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:10:02 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/WeightConversion.java
+++ src/coreprogramming/programmingelements/level2/WeightConversion.java
@@ -1,13 +1,25 @@
 package coreprogramming.programmingelements.level2;
 
 import java.util.Scanner;
+
+/*
+ * This program converts weight from pounds to kilograms.
+ */
 public class WeightConversion {
+
     public static void main(String[] args) {
+
+        // Taking weight input in pounds from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter weight in pounds: ");
         float weight = input.nextFloat();
+
+        // Converting weight to kilograms
         double kg = weight * 2.2;
-        System.out.println("The weight of the person in pounds is " +weight +" lb " +" and in kg is " +kg +" kg.");
+
+        // Displaying the weight in pounds and kilograms
+        System.out.println("The weight of the person in pounds is " + weight +
+                " lb and in kg is " + kg + " kg.");
     }
 }
 
