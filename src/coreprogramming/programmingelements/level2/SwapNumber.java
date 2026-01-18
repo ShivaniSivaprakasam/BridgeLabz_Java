fddb703c6f08d038711921f513d3d28005c1524a
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:08:05 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/SwapNumber.java
+++ src/coreprogramming/programmingelements/level2/SwapNumber.java
@@ -1,18 +1,33 @@
 package coreprogramming.programmingelements.level2;
 
 import java.util.Scanner;
+
+/*
+ * This program swaps two numbers
+ * using a temporary variable.
+ */
 public class SwapNumber {
+
     public static void main(String[] args) {
+
+        // Taking two numbers as input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number (number1): ");
         int number1 = input.nextInt();
+
         System.out.print("Enter number (number2): ");
         int number2 = input.nextInt();
-        System.out.println("Original(number1) : " +number1);
-        System.out.println("Original(number2) : " +number2);
+
+        // Displaying original numbers
+        System.out.println("Original(number1) : " + number1);
+        System.out.println("Original(number2) : " + number2);
+
+        // Swapping numbers using a temporary variable
         int temp = number1;
         number1 = number2;
         number2 = temp;
-        System.out.println("The swapped numbers are " +number1 + " and " + number2 + ".");
+
+        // Displaying swapped numbers
+        System.out.println("The swapped numbers are " + number1 + " and " + number2 + ".");
     }
 }
