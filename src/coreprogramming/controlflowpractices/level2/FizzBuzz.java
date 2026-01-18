e19e013b938eb6e56a4ac0812b8f1b5fe66882a0
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:28:41 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/FizzBuzz.java
+++ src/coreprogramming/controlflowpractices/level2/FizzBuzz.java
@@ -1,32 +1,45 @@
 package coreprogramming.controlflowpractices.level2;
+
 import java.util.Scanner;
+
 public class FizzBuzz {
+
+    /*
+     * This program implements the FizzBuzz logic for a single number input by the user.
+     * - If the number is divisible by 3 and 5, it prints "FizzBuzz".
+     * - If divisible only by 3, it prints "Fizz".
+     * - If divisible only by 5, it prints "Buzz".
+     * - Otherwise, it reports whether the number is positive, negative, or zero.
+     */
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        Scanner input = new Scanner(System.in); // Scanner object to get user input
+
+        // Prompt the user to enter a number
         System.out.print("Enter a number: ");
-        int number =  input.nextInt();
-        // Checking if the number is greater than zero 
-        if ( number > 0 ){
-            // Setting loop to find if the number is divisible by 3 and 5
-            if (number % 3 == 0 && number % 5 == 0){
-                System.out.println("FizzBuzz");
+        int number = input.nextInt();
+
+        // Check if the number is positive
+        if (number > 0) {
+
+            // Check divisibility conditions
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("FizzBuzz"); // Divisible by both 3 and 5
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz"); // Divisible only by 3
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz"); // Divisible only by 5
+            } else {
+                System.out.println(number + " is positive."); // Not divisible by 3 or 5
             }
-            else if (number % 5 == 0){
-                System.out.println("Buzz");
-            }
-            else if (number % 3 == 0){
-                System.out.println("FizzBuzz");
-            }
-            else{
-                System.out.println(number + " is positive.");
-            }
-        }
-        else if ( number < 0 ){
-            System.out.println(number + " is negative.");
-        }
-        else{
-            System.out.println(number + " is zero.");
+
+        } else if (number < 0) {
+            System.out.println(number + " is negative."); // Negative number
+
+        } else {
+            System.out.println(number + " is zero."); // Number is zero
         }
     }
 }
+
