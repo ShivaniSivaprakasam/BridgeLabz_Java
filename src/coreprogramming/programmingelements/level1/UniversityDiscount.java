38ddd7775ddb4257e57c79fcbbe49a1c9e017c29
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:01:41 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/UniversityDiscount.java
+++ src/coreprogramming/programmingelements/level1/UniversityDiscount.java
@@ -1,15 +1,32 @@
 package coreprogramming.programmingelements.level1;
 
 import java.util.Scanner;
+
+/*
+ * This program calculates the discount amount
+ * and the final fee after applying the discount.
+ */
 public class UniversityDiscount {
+
     public static void main(String[] args) {
+
+        // Taking fee input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the fee in INR: ");
         double fee = input.nextDouble();
+
+        // Taking discount percentage input from the user
         System.out.print("Enter the discount percentage (divide by 100) : ");
         double discountPercent = input.nextDouble();
-        double discount = fee *  discountPercent;
-        double discountedFee = fee -  discount;
-        System.out.println("The discount amount is INR " +discount + " and final discounted fee is INR " +discountedFee + ".");
+
+        // Calculating discount amount
+        double discount = fee * discountPercent;
+
+        // Calculating final discounted fee
+        double discountedFee = fee - discount;
+
+        // Displaying discount and final fee
+        System.out.println("The discount amount is INR " + discount +
+                " and final discounted fee is INR " + discountedFee + ".");
     }
 }
