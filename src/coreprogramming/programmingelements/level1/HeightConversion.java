1144a304dc0338d29f896198ca3b331fa28ebce1
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 17:56:57 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/HeightConversion.java
+++ src/coreprogramming/programmingelements/level1/HeightConversion.java
@@ -1,13 +1,26 @@
 package coreprogramming.programmingelements.level1;
 
 import java.util.Scanner;
+
+/*
+ * This program converts height from centimeters to feet and inches.
+ */
 public class HeightConversion {
+
     public static void main(String[] args) {
+
+        // Taking height input in centimeters
         Scanner input = new Scanner(System.in);
         System.out.print("Enter height in cm: ");
         double height = input.nextDouble();
-        double inch = height/2.54;
-        double feet = inch/12;
-        System.out.print("your Height in cm is " +height + " cm while in feet is " +feet + " ft and inches is " +inch +"″");
+
+        // Converting cm to inches and feet
+        double inch = height / 2.54;
+        double feet = inch / 12;
+
+        // Displaying the converted values
+        System.out.print("Your height in cm is " + height +
+                " cm while in feet is " + feet +
+                " ft and inches is " + inch + "″");
     }
 }
