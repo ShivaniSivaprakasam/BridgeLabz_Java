7e517dc856e9d780e5f78d9201e94edff0e92c6c
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 17:59:45 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/PerimeterOfSquare.java
+++ src/coreprogramming/programmingelements/level1/PerimeterOfSquare.java
@@ -1,12 +1,25 @@
 package coreprogramming.programmingelements.level1;
 
 import java.util.Scanner;
+
+/*
+ * This program calculates the length of one side of a square
+ * using the given perimeter.
+ */
 public class PerimeterOfSquare {
+
     public static void main(String[] args) {
+
+        // Taking perimeter input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the perimeter of square in cm: ");
         double perimeter = input.nextDouble();
-        double side = perimeter/4;
-        System.out.println("The length of the size is " +side +" cm " + " whose perimeter is " +perimeter +" cm.");
+
+        // Calculating the side length of the square
+        double side = perimeter / 4;
+
+        // Displaying the result
+        System.out.println("The length of the size is " + side +
+                " cm whose perimeter is " + perimeter + " cm.");
     }
 }
