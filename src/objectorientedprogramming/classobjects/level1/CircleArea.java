70a49ac26dfa5caf144941b3dc3b1d922f3eafe5
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:46:46 +0530

    [Shivani] : Added : mulit-comment lines to the code

--- src/objectorientedprogramming/classobjects/level1/CircleArea.java
+++ src/objectorientedprogramming/classobjects/level1/CircleArea.java
@@ -1,4 +1,17 @@
 package objectorientedprogramming.classobjects.level1;
+/*
+ * This program demonstrates the use of classes and objects in Java.
+ * 
+ * 1. The Circle class stores the radius of a circle as a private field.
+ *    It has a constructor to initialize the radius, a getter to access it,
+ *    and a setter to update it (only if the value is positive).
+ *
+ * 2. The CircleArea class calculates and displays the area of a Circle object.
+ *    - The area method takes a Circle object and returns its area using the formula π*r^2.
+ *    - In the main method, the user inputs the radius, a Circle object is created,
+ *      and its area is printed.
+ */
+
 import  java.util.Scanner;
 // Created a circle class
 class Circle{
