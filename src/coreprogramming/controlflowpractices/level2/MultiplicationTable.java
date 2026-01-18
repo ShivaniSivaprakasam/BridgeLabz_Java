00748c927c9420bf04a87f110b9ab58017afe06e
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:40:33 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/MultiplicationTable.java
+++ src/coreprogramming/controlflowpractices/level2/MultiplicationTable.java
@@ -1,16 +1,32 @@
 package coreprogramming.controlflowpractices.level2;
+
 import java.util.Scanner;
+
 public class MultiplicationTable {
+
+    /*
+     * This program prints the multiplication table of a number
+     * for the multiples from 6 to 9.
+     * Steps:
+     * 1. Take user input for the number.
+     * 2. Use a for loop to multiply the number by 6, 7, 8, and 9.
+     * 3. Print each result in a formatted way.
+     */
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Gettting user input
+
+        Scanner input = new Scanner(System.in); // Scanner for user input
+
+        // Prompt user to enter a number
         System.out.print("Enter a number: ");
         int number = input.nextInt();
-        int table = 0;
-        // Setting loop conditions to get multipes from 6 to 9
-        for ( int i = 6; i <= 9; i++ ){
-            table = number * i;
-            System.out.println("The multiplication table for the number " +number +" from 6 to 9 is: " +table);
+
+        System.out.println("Multiplication table of " + number + " from 6 to 9:");
+
+        // Loop to calculate and print multiples from 6 to 9
+        for (int i = 6; i <= 9; i++) {
+            int table = number * i;
+            System.out.println(number + " x " + i + " = " + table);
         }
     }
 }
