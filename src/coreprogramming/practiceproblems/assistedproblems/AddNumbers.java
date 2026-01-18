301a8621eddfa78f25e2394b56f6a5c16955eb92
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 14:03:04 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/practiceproblems/assistedproblems/AddNumbers.java
+++ src/coreprogramming/practiceproblems/assistedproblems/AddNumbers.java
@@ -1,13 +1,30 @@
 package coreprogramming.practiceproblems.assistedproblems;
+
 import java.util.Scanner;
+
+/**
+ * AddNumbers class
+ * ----------------
+ * This program takes two integers as input from the user
+ * and prints their sum.
+ */
 public class AddNumbers {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Input first number
         System.out.print("Enter first number: ");
         int firstNumber = input.nextInt();
+
+        // Input second number
         System.out.print("Enter second number: ");
         int secondNumber = input.nextInt();
-        int sum =  firstNumber + secondNumber;
+
+        // Calculate sum
+        int sum = firstNumber + secondNumber;
+
+        // Display result
         System.out.println("The sum is: " + sum);
     }
 }
