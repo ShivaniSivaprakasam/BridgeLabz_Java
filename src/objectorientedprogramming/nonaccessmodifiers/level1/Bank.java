d30e5b40b8702ee729c17683ed49d48b3d2c8a51
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 21:56:57 +0530

    [Shivani] : Added : mulit-comment lines to the code

--- src/objectorientedprogramming/nonaccessmodifiers/level1/Bank.java
+++ src/objectorientedprogramming/nonaccessmodifiers/level1/Bank.java
@@ -40,7 +40,7 @@ class BankAccount {
     }
 }
 // Created a main class called bank
-public class bank {
+public class Bank {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
