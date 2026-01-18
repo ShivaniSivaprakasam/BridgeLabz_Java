ba7e4eba60773e12342b88d714825531041a0b4e
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 14:04:29 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/practiceproblems/assistedproblems/Greetings.java
+++ src/coreprogramming/practiceproblems/assistedproblems/Greetings.java
@@ -1,7 +1,14 @@
 package coreprogramming.practiceproblems.assistedproblems;
 
+/**
+ * Greetings class
+ * ----------------
+ * This program prints a welcome message to the console.
+ */
 public class Greetings {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        // Print the welcome message
         System.out.println("Welcome to Bridgelabz!");
     }
 }
