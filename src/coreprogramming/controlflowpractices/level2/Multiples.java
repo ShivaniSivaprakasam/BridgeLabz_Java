948a74f60c091e90dd180b76cb55f8797611c6bf
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:39:05 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/Multiples.java
+++ src/coreprogramming/controlflowpractices/level2/Multiples.java
@@ -1,22 +1,38 @@
 package coreprogramming.controlflowpractices.level2;
-import  java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Multiples {
+
+    /*
+     * This program finds all divisors (factors) of a number entered by the user.
+     * Steps:
+     * 1. Take a number input less than or equal to 100 from the user.
+     * 2. Use a for loop to check divisibility from 1 to 100.
+     * 3. Print all numbers that divide the input evenly.
+     */
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        Scanner input = new Scanner(System.in); // Scanner for user input
+
+        // Prompt user to enter a number
         System.out.print("Enter a number: ");
         int number = input.nextInt();
-        // Setting while loop to get only number less than 100
-        while ( number > 100 ){
-            System.out.print("Enter a number less than 100: ");
+
+        // Ensure the number is <= 100
+        while (number > 100) {
+            System.out.print("Enter a number less than or equal to 100: ");
             number = input.nextInt();
         }
-        // Checking divisibility
-        for ( int i = 1; i <= 100; i++){
-            if ( number % i == 0){
+
+        System.out.println("The divisors of " + number + " are:");
+
+        // Loop to find all divisors
+        for (int i = 1; i <= 100; i++) {
+            if (number % i == 0) { // Check if i divides the number
                 System.out.println(i);
             }
         }
     }
 }
-
