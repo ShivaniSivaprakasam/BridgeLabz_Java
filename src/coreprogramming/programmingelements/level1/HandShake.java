30e9e67197e0161d5923aec7be85b61313248c79
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 17:56:10 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/HandShake.java
+++ src/coreprogramming/programmingelements/level1/HandShake.java
@@ -1,13 +1,24 @@
 package coreprogramming.programmingelements.level1;
 
-import  java.util.Scanner;
+import java.util.Scanner;
+
+/*
+ * This program calculates the number of possible handshakes
+ * among a given number of students.
+ */
 public class HandShake {
+
     public static void main(String[] args) {
+
+        // Taking input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of students: ");
         int numberOfStudents = input.nextInt();
+
+        // Calculating handshakes using formula n(n-1)/2
         float combination = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        // Displaying the result
         System.out.println("The number of possible handshakes is: " + combination);
     }
 }
-
