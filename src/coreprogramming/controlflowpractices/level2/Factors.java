47baed0ac1d6591d95e3073fcbf7aa5ddb6921aa
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:26:33 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/Factors.java
+++ src/coreprogramming/controlflowpractices/level2/Factors.java
@@ -1,24 +1,49 @@
 package coreprogramming.controlflowpractices.level2;
+
 import java.util.Scanner;
+
 public class Factors {
-    // Creating a method to find factors of a number
-    public static void findingFactors(int N){
-        while ( N < 0 ) {
+
+    /*
+     * This program finds all the factors of a given positive integer.
+     * It prompts the user for input if a negative number is entered.
+     * Each factor is printed on a new line.
+     */
+
+    // Method to find and print factors of a number
+    public static void findingFactors(int N) {
+
+        // Loop to ensure N is positive
+        while (N < 0) {
+
+            // Asking user for a positive integer
             System.out.print("Enter a positive integer: ");
             N = new Scanner(System.in).nextInt();
         }
-        for ( int i = 1; i <= N; i++ ){
-            if ( N % i == 0 ){
-                System.out.println(i);
+
+        // Loop from 1 to N to check for factors
+        for (int i = 1; i <= N; i++) {
+
+            // If i divides N completely, it is a factor
+            if (N % i == 0) {
+                System.out.println(i); // Print the factor
             }
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        // Creating a Scanner object for user input
         Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        // Prompting user to enter a number
         System.out.print("Enter a number: ");
         int N = input.nextInt();
-        // Calling method
+
+        // Calling the method to find and display factors
         findingFactors(N);
+
+        // Closing the Scanner
+        input.close();
     }
 }
