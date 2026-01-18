5c85bb577301c595b50b2bc67ff34ce94fe03d8e
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:00:20 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/Profit.java
+++ src/coreprogramming/programmingelements/level1/Profit.java
@@ -1,12 +1,31 @@
 package coreprogramming.programmingelements.level1;
 
+/*
+ * This program calculates the profit and profit percentage
+ * based on the cost price and selling price.
+ */
 public class Profit {
+
     public static void main(String[] args) {
+
+        // Cost Price
         double CP = 129;
+
+        // Selling Price
         double SP = 191;
+
+        // Calculating profit
         double profit = SP - CP;
-        double profitPercentage = (profit/CP) * 100;
-        System.out.println("The Cost price is INR " + CP + " and the Selling Price is INR " + SP + ".");
-        System.out.println("The profit is INR " + profit + " and the Task21512.Profit Percentage is: " + profitPercentage + "%.");
+
+        // Calculating profit percentage
+        double profitPercentage = (profit / CP) * 100;
+
+        // Displaying cost price and selling price
+        System.out.println("The Cost price is INR " + CP +
+                " and the Selling Price is INR " + SP + ".");
+
+        // Displaying profit and profit percentage
+        System.out.println("The profit is INR " + profit +
+                " and the Profit Percentage is: " + profitPercentage + "%.");
     }
 }
