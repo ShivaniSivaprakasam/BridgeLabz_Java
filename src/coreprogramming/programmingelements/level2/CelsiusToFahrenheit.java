e508e9ec5c6c0f4345a70e79fb7a09c4a7a8e93f
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:02:57 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/CelsiusToFahrenheit.java
+++ src/coreprogramming/programmingelements/level2/CelsiusToFahrenheit.java
@@ -1,12 +1,26 @@
 package coreprogramming.programmingelements.level2;
 
 import java.util.Scanner;
+
+/*
+ * This program converts temperature from
+ * Celsius to Fahrenheit.
+ */
 public class CelsiusToFahrenheit {
+
     public static void main(String[] args) {
+
+        // Taking temperature input in Celsius
         Scanner input = new Scanner(System.in);
         System.out.print("Enter temperature in Celsius: ");
         double celsius = input.nextDouble();
-        double fahrenheitResult = (celsius * 9/5) + 32;
-        System.out.println("The " +celsius +" celsius is " +fahrenheitResult +" fahrenheit.");
+
+        // Converting Celsius to Fahrenheit
+        double fahrenheitResult = (celsius * 9 / 5) + 32;
+
+        // Displaying the converted temperature
+        System.out.println("The " + celsius +
+                " celsius is " + fahrenheitResult + " fahrenheit.");
     }
 }
+
