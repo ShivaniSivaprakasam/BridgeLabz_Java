8447a234bfedd4023e63852ea06d788fcf867ec0
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 18:06:10 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level2/QuotientReminder.java
+++ src/coreprogramming/programmingelements/level2/QuotientReminder.java
@@ -1,15 +1,32 @@
 package coreprogramming.programmingelements.level2;
 
 import java.util.Scanner;
+
+/*
+ * This program calculates the quotient and remainder
+ * of two given numbers.
+ */
 public class QuotientReminder {
+
     public static void main(String[] args) {
+
+        // Taking input values from the user
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter first number: ");
         float number1 = input.nextFloat();
+
         System.out.print("Enter second number: ");
-        float  number2 = input.nextFloat();
+        float number2 = input.nextFloat();
+
+        // Calculating quotient and remainder
         float quotient = number1 / number2;
         float remainder = number1 % number2;
-        System.out.println("The quotient is " +quotient +" and remainder is " +remainder +" of two numbers " +number1 +" and " +number2);
+
+        // Displaying the result
+        System.out.println("The quotient is " + quotient +
+                " and remainder is " + remainder +
+                " of two numbers " + number1 + " and " + number2);
     }
 }
+
