38f4406678978a0d9ef2fc2936fdb257efe760d1
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 12:29:37 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/controlflowpractices/level2/FizzBuzzFor.java
+++ src/coreprogramming/controlflowpractices/level2/FizzBuzzFor.java
@@ -1,29 +1,42 @@
 package coreprogramming.controlflowpractices.level2;
+
 import java.util.Scanner;
+
 public class FizzBuzzFor {
+
+    /*
+     * This program prints FizzBuzz for numbers from 1 to the number entered by the user.
+     * Rules:
+     * - If a number is divisible by 3 and 5, print "FizzBuzz".
+     * - If divisible only by 3, print "Fizz".
+     * - If divisible only by 5, print "Buzz".
+     * - Otherwise, print the number itself.
+     */
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        // Getting user input
+
+        Scanner input = new Scanner(System.in); // Scanner object to get user input
+
+        // Prompt the user to enter a number
         System.out.print("Enter a number: ");
-        int number =  input.nextInt();
+        int number = input.nextInt();
+
+        // Loop through numbers from 1 to 'number'
         int i = 1;
-        // Condition to check if the number is greater than 1
         while (i <= number) {
-            // Condition to check divisibility by 3 and 5
+
+            // Check divisibility by 3 and 5
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println("FizzBuzz"); // Divisible by both 3 and 5
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz"); // Divisible only by 3
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz"); // Divisible only by 5
+            } else {
+                System.out.println(i); // Not divisible by 3 or 5
             }
-            else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }
-            else {
-                System.out.println(i);
-            }
-            // Incrementing i
-            i++;
+
+            i++; // Increment counter
         }
     }
 }
