7e3a60d21cf447b1562d3a9d903b9a9c593006e9
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 13:29:14 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/javamethods/level1/HandShakes.java
+++ src/coreprogramming/javamethods/level1/HandShakes.java
@@ -1,16 +1,43 @@
 package coreprogramming.javamethods.level1;
+
 import java.util.Scanner;
+
 public class HandShakes {
-    public static float handShake( int n ){
-        float combination = ( n * ( n - 1 ) ) / 2;
+
+    /*
+     * This method calculates the total number of possible handshakes
+     * among 'n' students.
+     *
+     * Formula:
+     * Handshakes = n × (n − 1) / 2
+     *
+     * Parameter:
+     * - n : number of students
+     *
+     * Returns:
+     * - total number of handshakes
+     */
+    public static float handShake(int n) {
+
+        // Apply handshake formula
+        float combination = (n * (n - 1)) / 2;
+
         return combination;
     }
-    public static void main(String[] args){
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of students: ");
-       int n = input.nextInt();
-       float handShakes = handShake(n);
-       System.out.println("The number of possible handshakes is: " +handShakes);
-   }
-}
 
+    public static void main(String[] args) {
+
+        // Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+        // Read number of students
+        System.out.print("Enter number of students: ");
+        int n = input.nextInt();
+
+        // Calculate handshakes
+        float handShakes = handShake(n);
+
+        // Display result
+        System.out.println("The number of possible handshakes is: " + handShakes);
+    }
+}
