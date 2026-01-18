2f07d3e5489f39f036cfc1237abe66512b2dd3c1
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 14:05:04 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/practiceproblems/assistedproblems/TempConversion.java
+++ src/coreprogramming/practiceproblems/assistedproblems/TempConversion.java
@@ -1,12 +1,25 @@
 package coreprogramming.practiceproblems.assistedproblems;
 
 import java.util.Scanner;
+
+/**
+ * TempConversion class
+ * --------------------
+ * This program converts a temperature from Celsius to Fahrenheit.
+ */
 public class TempConversion {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Prompt user for temperature in Celsius
         System.out.print("Enter temperature in Celsius: ");
         double celsius = input.nextDouble();
-        double fahrenheit = (celsius * 9/5) + 32;
+
+        // Convert Celsius to Fahrenheit
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        // Display the result
         System.out.println("The temperature in Fahrenheit is: " + fahrenheit + " degrees F");
     }
 }
