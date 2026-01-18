ed73c79bb3635a55633da092c323e97382228a1a
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-18 17:58:09 +0530

    [Shivani] : Added : comment lines to the code

--- src/coreprogramming/programmingelements/level1/MilesConversion.java
+++ src/coreprogramming/programmingelements/level1/MilesConversion.java
@@ -1,10 +1,27 @@
 package coreprogramming.programmingelements.level1;
 
+/*
+ * This program converts a fixed distance in kilometers
+ * into miles using a constant multiplier.
+ */
 public class MilesConversion {
+
     public static void main(String[] args) {
+
+        // Distance in kilometers
         double Km = 10.8;
+
+        // Conversion factor from kilometers to miles
         final double multiplier = 1.6;
+
+        /*
+         * Calculating the distance in miles
+         * by multiplying kilometers with the multiplier
+         */
         double miles = Km * multiplier;
-        System.out.println("The distance of " +Km + " km in miles is: " + miles + " mi.");
+
+        // Displaying the converted distance
+        System.out.println("The distance of " + Km +
+                " km in miles is: " + miles + " mi.");
     }
 }
